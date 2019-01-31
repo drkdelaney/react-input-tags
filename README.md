f9e51e39b988414a839a162db28b43bafcbfa23f
Derek Delaney <derek.delaney@nike.com>
2019-01-31 15:49:21 -0800

    added input tags component and updated settings

--- README.md
+++ README.md
@@ -3,16 +3,16 @@
 > An input box with tags.
 [Demo page](https://pages.github.io/derekedelaney/react-input-tags/storybook-static)
 
-[![NPM](https://img.shields.io/npm/v/react-input-tags.svg)](https://www.npmjs.com/package/react-input-tags) [![JavaScript Style Guide](https://img.shields.io/badge/code_style-standard-brightgreen.svg)](https://standardjs.com)
+[![NPM](https://img.shields.io/npm/v/react-input-tags.svg)](https://www.npmjs.com/package/@derekedelaney/react-input-tags) [![JavaScript Style Guide](https://img.shields.io/badge/code_style-standard-brightgreen.svg)](https://standardjs.com)
 
 ## Install
 
 ```bash
-npm install --save react-input-tags
+npm install --save @derekedelaney/react-input-tags
 ```
 OR
 ```bash
-yarn add react-input-tags
+yarn add @derekedelaney/react-input-tags
 ```
 
 ## Usage
--- src/index.js
+++ src/index.js
@@ -1,22 +1,160 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import cx from 'classnames';
+import Close from './close-button';
 
-import styles from './styles.css'
+import styles from './styles.css';
 
-export default class ExampleComponent extends Component {
-  static propTypes = {
-    text: PropTypes.string
-  }
+export default class InputTags extends Component {
+    static propTypes = {
+        tags: PropTypes.arrayOf(
+            PropTypes.shape({
+                id: PropTypes.oneOfType([PropTypes.string, PropTypes.number])
+                    .isRequired,
+                text: PropTypes.string.isRequired
+            })
+        ).isRequired,
+        onDeleteTag: PropTypes.func,
+        onNewTag: PropTypes.func,
+        keyCodes: PropTypes.arrayOf(PropTypes.number),
+        onInputChange: PropTypes.func,
+        onEmptyDelete: PropTypes.func,
+        clearInputOnNewTag: PropTypes.bool,
+        inputPlaceholderText: PropTypes.string,
+        borderClass: PropTypes.string,
+        tagContainerClass: PropTypes.string,
+        tagClass: PropTypes.string,
+        closeClass: PropTypes.string,
+        tagInputClass: PropTypes.string
+    };
 
-  render() {
-    const {
-      text
-    } = this.props
+    static defaultProps = {
+        onDeleteTag: () => {},
+        keyCodes: [],
+        onInputChange: () => {},
+        onEmptyDelete: () => {},
+        onNewTag: () => {},
+        clearInputOnNewTag: false,
+        borderClass: null,
+        tagContainerClass: null,
+        tagClass: null,
+        closeClass: null,
+        tagInputClass: null
+    };
 
-    return (
-      <div className={styles.test}>
-        Example Component: {text}
-      </div>
-    )
-  }
+    state = {
+        didPaste: false
+    };
+
+    componentDidUpdate(prevProps) {
+        if (
+            prevProps.tags.length < this.props.tags.length &&
+            this.props.clearInputOnNewTag
+        ) {
+            this.clearInput();
+            this.props.onInputChange('');
+        }
+    }
+
+    keyCodeMap = {
+        '\t': 9,
+        '\n': 13,
+        ' ': 32,
+        ',': 188
+    };
+
+    handleChange = e => {
+        if (this.state.didPaste) {
+            this.setState({ didPaste: false });
+            this.parsePaste(e.target.value);
+            this.props.onInputChange(e.target.value);
+            this.clearInput();
+        } else {
+            this.props.onInputChange(e.target.value);
+        }
+    };
+
+    handleKeyDown = e => {
+        if (this.props.keyCodes.indexOf(e.keyCode) > -1) {
+            if (e.keyCode === 9) e.preventDefault();
+            this.props.onNewTag(e.target.value.trim());
+        }
+        if (e.keyCode === 8 && e.target.value === '') {
+            this.props.onEmptyDelete();
+        }
+    };
+
+    handleKeyUp = e => {
+        if (
+            this.props.clearInputOnNewTag &&
+            this.props.keyCodes.indexOf(e.keyCode) > -1
+        ) {
+            this.clearInput();
+        }
+    };
+
+    handlePaste = () => {
+        this.setState({ didPaste: true });
+    };
+
+    parsePaste = value => {
+        for (let i = 0, j = 0; i < value.length; i++) {
+            if (this.props.keyCodes.indexOf(this.keyCodeMap[value[i]]) > -1) {
+                this.props.onNewTag(value.slice(j, i).trim());
+                j = i + 1;
+            } else if (i === value.length - 1) {
+                this.props.onNewTag(value.slice(j, value.length).trim());
+            }
+        }
+    };
+
+    clearInput = () => {
+        this.input.value = '';
+    };
+
+    render() {
+        const {
+            tags,
+            onDeleteTag,
+            inputPlaceholderText,
+            borderClass,
+            tagContainerClass,
+            tagClass,
+            closeClass,
+            tagInputClass
+        } = this.props;
+
+        return (
+            <div className={cx(styles.border, borderClass)}>
+                <div className={cx(styles.tagContainer, tagContainerClass)}>
+                    {tags &&
+                        tags.map((tag, index) => (
+                            <span
+                                key={tag.id}
+                                className={cx(styles.tag, tagClass)}
+                            >
+                                {tag.text}
+                                <Close
+                                    size={12}
+                                    className={cx(styles.close, closeClass)}
+                                    onClick={() => {
+                                        onDeleteTag(tag, index);
+                                    }}
+                                />
+                            </span>
+                        ))}
+                    <input
+                        ref={node => (this.input = node)}
+                        className={cx(styles.tagInput, tagInputClass)}
+                        type="search"
+                        onChange={this.handleChange}
+                        onKeyDown={this.handleKeyDown}
+                        onKeyUp={this.handleKeyUp}
+                        placeholder={inputPlaceholderText}
+                        onPaste={this.handlePaste}
+                    />
+                </div>
+            </div>
+        );
+    }
 }
